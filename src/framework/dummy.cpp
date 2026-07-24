fbe6c5004aa56c5e228d978992e899a1a9b79282
Phonaputer <njerome@uwalumni.com>
2026-07-24 18:17:14 +0900

    Add Luau and write hello world program where the hello world text is printed from Luau.

--- src/framework/dummy.cpp
+++ src/framework/dummy.cpp
@@ -1,7 +1,31 @@
 #include "framework/dummy.hpp"
 
 #include <iostream>
+#include <luacode.h>
+#include <lualib.h>
+#include <string>
 
 void hello_world() {
-  std::cout << "Hello, world!" << "\n";
+  const std::string source = R"(print("Hello from Lua!"))";
+
+  lua_State *L = luaL_newstate();
+  luaL_openlibs(L);
+
+  size_t bytecodeSize = 0;
+  char *bytecode = luau_compile(source.c_str(), source.length(), NULL, &bytecodeSize);
+
+  if (luau_load(L, "HelloWorld", bytecode, bytecodeSize, 0) != LUA_OK) {
+    std::cerr << "Error loading string: " << lua_tostring(L, -1) << std::endl;
+    lua_close(L);
+    free(bytecode);
+    return;
+  }
+
+  free(bytecode);
+
+  if (lua_pcall(L, 0, 0, 0) != LUA_OK) {
+    std::cerr << "Error running code: " << lua_tostring(L, -1) << std::endl;
+  }
+
+  lua_close(L);
 }
