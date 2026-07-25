f5a28c8faff5d03416524062177311f119517d37
Phonaputer <njerome@uwalumni.com>
2026-07-25 14:43:09 +0900

    Add LuaBridge3 library to help out serving C++ functions for Lua.

--- src/framework/dummy.cpp
+++ src/framework/dummy.cpp
@@ -1,15 +1,32 @@
-#include "framework/dummy.hpp"
 
-#include <iostream>
 #include <luacode.h>
 #include <lualib.h>
+
+#include <LuaBridge/LuaBridge.h>
+
+#include "framework/dummy.hpp"
+#include <iostream>
 #include <string>
 
+void print_from_cpp(std::string str) {
+  std::cout << str << "\n";
+}
+
 void hello_world() {
-  const std::string source = R"(print("Hello from Lua!"))";
+  const std::string source = R"(
+  local s = "Hello! Now I will count to ten... 1"
+
+  for i = 2, 10 do
+      s = s .. " " .. i
+  end
+
+  test.print_from_cpp(s .. "!")
+  )";
 
   lua_State *L = luaL_newstate();
   luaL_openlibs(L);
+
+  luabridge::getGlobalNamespace(L).beginNamespace("test").addFunction("print_from_cpp", print_from_cpp).endNamespace();
 
   size_t bytecodeSize = 0;
   char *bytecode = luau_compile(source.c_str(), source.length(), NULL, &bytecodeSize);
